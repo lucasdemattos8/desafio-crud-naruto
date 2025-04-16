8d6843803f1f77f7ddc8c57f9b66d06f7bb4980c
LucasMiranda <ldemattosmiranda@gmail.com>
2025-04-16 15:15:01 -0300

    Inicialização do Projeto Spring

---
+++ src/main/java/com/db/desafio_naruto/DesafioNarutoApplication.java
@@ -0,0 +1,13 @@
+package com.db.desafio_naruto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNarutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNarutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/desafio_naruto/DesafioNarutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.desafio_naruto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNarutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
