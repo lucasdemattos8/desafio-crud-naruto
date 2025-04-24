71a49912ddef26dfbbeac95dbba29c87998558d0
LucasMiranda <ldemattosmiranda@gmail.com>
2025-04-24 11:04:03 -0300

    Adiciona logs aos serviço de Autenticação

--- src/main/java/com/db/desafio_naruto/application/service/AuthService.java
+++ src/main/java/com/db/desafio_naruto/application/service/AuthService.java
@@ -3,19 +3,30 @@ package com.db.desafio_naruto.application.service;
 import org.springframework.stereotype.Service;
 
 import com.db.desafio_naruto.application.port.in.AutenticacaoUseCase;
+import com.db.desafio_naruto.application.port.out.LogPort;
 import com.db.desafio_naruto.application.port.out.SecurityPort;
 
 @Service
 public class AuthService implements AutenticacaoUseCase {
     
     private final SecurityPort securityPort;
+    private final LogPort logPort;
 
-    public AuthService(SecurityPort securityPort) {
+    public AuthService(SecurityPort securityPort, LogPort logPort) {
         this.securityPort = securityPort;
+        this.logPort = logPort;
     }
 
     @Override
     public String autenticar() {
-        return securityPort.gerarToken();
+        logPort.info("Iniciando processo de autenticação");
+        try {
+            String token = securityPort.gerarToken();
+            logPort.debug("Token gerado com sucesso");
+            return token;
+        } catch (Exception e) {
+            logPort.error("Erro ao gerar token de autenticação", e);
+            throw e;
+        }
     }
 }
