2b0f422cd5913e6761972b0d40fa7e11fce0fd04
LucasMiranda <ldemattosmiranda@gmail.com>
2025-04-24 10:05:57 -0300

    Retira construtor e setters inacessiveis de DTO

--- src/main/java/com/db/desafio_naruto/application/port/in/dto/PersonagemDTO.java
+++ src/main/java/com/db/desafio_naruto/application/port/in/dto/PersonagemDTO.java
@@ -14,9 +14,6 @@ public class PersonagemDTO {
     private int chakra;
     private TipoNinja tipoNinja;
 
-    public PersonagemDTO() {
-    }
-
     public PersonagemDTO(Long id, String nome, int idade, String aldeia,
             List<String> jutsus, int chakra, TipoNinja tipoNinja) {
         this.id = id;
@@ -32,55 +29,29 @@ public class PersonagemDTO {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public int getIdade() {
         return idade;
     }
 
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
     public String getAldeia() {
         return aldeia;
-    }
-
-    public void setAldeia(String aldeia) {
-        this.aldeia = aldeia;
     }
 
     public List<String> getJutsus() {
         return jutsus;
     }
 
-    public void setJutsus(List<String> jutsus) {
-        this.jutsus = jutsus;
-    }
-
     public int getChakra() {
         return chakra;
     }
 
-    public void setChakra(int chakra) {
-        this.chakra = chakra;
-    }
 
     public TipoNinja getTipoNinja() {
         return tipoNinja;
     }
 
-    public void setTipoNinja(TipoNinja tipoNinja) {
-        this.tipoNinja = tipoNinja;
-    }
 }
