b46b8b89769bd45717b7426dbe607d33e77b9dcc
LucasMiranda <ldemattosmiranda@gmail.com>
2025-04-24 10:05:29 -0300

    Adiciona tesde integração para o endpoint de Autenticação

---
+++ src/test/java/com/db/desafio_naruto/infrastructure/adapter/in/rest/AuthControllerIT.java
@@ -0,0 +1,47 @@
+package com.db.desafio_naruto.infrastructure.adapter.in.rest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.jayway.jsonpath.JsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class AuthControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void deveGerarTokenComSucesso() throws Exception {
+        mockMvc.perform(post("/api/v1/auth"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.token", not(emptyString())));
+    }
+
+    @Test
+    void deveValidarTokenGerado() throws Exception {
+        String response = mockMvc.perform(post("/api/v1/auth"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+
+        String token = JsonPath.read(response, "$.token");
+
+        mockMvc.perform(get("/api/v1/personagens")
+                .header("Authorization", "Bearer " + token))
+            .andExpect(status().isOk());
+    }
+}
